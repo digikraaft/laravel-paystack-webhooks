c5e8a786b1dc43fc73ff5eb0da5ff4c56416c1e7
Tim Oladoyinbo <timoladoyinbo@Tims-MBP-2.lan>
2024-04-27 12:11:23 +0100

    webhook signature fix

--- src/Http/Controllers/WebhooksController.php
+++ src/Http/Controllers/WebhooksController.php
@@ -25,16 +25,18 @@ class WebhooksController extends Controller
     public function handleWebhook(Request $request): \Symfony\Component\HttpFoundation\Response
     {
         $payload = json_decode($request->getContent(), true);
+
         $method = 'handle'.Str::studly(str_replace('.', '_', $payload['event']));
 
         WebhookReceived::dispatch($payload);
 
         if (method_exists($this, $method)) {
+
             $response = $this->{$method}($payload);
 
             WebhookHandled::dispatch($payload);
 
-            return $response;
+            return $response ?? $this->successMethod();
         }
 
         return $this->missingMethod();
--- src/Http/Middleware/VerifyWebhookSignature.php
+++ src/Http/Middleware/VerifyWebhookSignature.php
@@ -11,13 +11,14 @@ class VerifyWebhookSignature
     public function handle($request, Closure $next): \Illuminate\Http\Response
     {
         // validate that callback is coming from Paystack
-        if ((! $request->isMethod('post')) || ! $request->header('HTTP_X_PAYSTACK_SIGNATURE', null)) {
+        if ((! $request->isMethod('post')) || ! $request->header('x-paystack-signature', null)) {
             throw new AccessDeniedHttpException("Invalid Request");
         }
 
         $input = $request->getContent();
+
         $paystack_key = config('paystackwebhooks.secret', env('PAYSTACK_SECRET'));
-        if ($request->header('HTTP_X_PAYSTACK_SIGNATURE') !== hash_hmac('sha512', $input, $paystack_key)) {
+        if ($request->header('x-paystack-signature') !== hash_hmac('sha512', $input, $paystack_key)) {
             throw new AccessDeniedHttpException("Access Denied");
         }
 
